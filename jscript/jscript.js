618098969850ea9cdba48d87e49125de4fa217a8
adrianminnaar <adrianminaar29@gmail.com>
2023-03-25 11:57:34 +0200

    update

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,13 +1,10 @@
-//create elements
-const form = document.getElementById('todoform');
-const todo = document.getElementById('newtodo');
-const todoListEl = document.getElementById('todos-list');
-//const notificationEl = document.querySelector('.notification');
+todoMain();
 
-let todos = JSON.parse(localStorage.getItem('todos'))|| [];
-//let EditTodoId = -1
-form.addEventListener('submit',function(event){
-  event.preventDefault();
+function todoMain(){
+  getElements();
+  addEventListener();
+  
+}
 
-  console.log('submit')
-})
+
+
